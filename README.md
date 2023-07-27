898347e53ae4db7a151815a35742edf22431fcae
G-Linares1 <gerrylinares@gmail.com>
2023-07-27 12:58:10 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Admin Dashboard
+
+### Links
+
+Component library used
+
+- https://ui.shadcn.com/
+
+Method for routing used
+
+- https://nextjs.org/docs/app/building-your-application/routing/route-groups
+
+Auth method
+
+- https://clerk.com/
---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+
+const SetUpPage = () => {
+  return (
+    <div className="p-4">
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
+};
+
+export default SetUpPage;
